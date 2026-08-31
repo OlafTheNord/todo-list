54c8dc16c7f21709deff378ebc4a12e9b47e9f4e
Артемов Александр <artemov.a23@rwb.ru>
2026-08-31 17:19:57 +0300

    Add first func

---
+++ SCHEMA/tasks/FUNCTION/task_add.sql
@@ -0,0 +1,31 @@
+REATE OR REPLACE FUNCTION tasks.task_add(author BIGINT,
+                                          title  VARCHAR(200),
+                                          body   TEXT)
+    RETURNS JSONB
+    LANGUAGE plpgsql
+SECURITY DEFINER
+    AS
+    $$
+DECLARE
+    _dt      TIMESTAMP WITHOUT TIME ZONE;
+    _error   VARCHAR(500);
+    _task_id BIGINT;
+BEGIN
+    _dt = NOW();
+
+    _error = CONCAT('author = ', author,
+                    ', title = ', title);
+
+    _task_id = NEXTVAL('tasks.task_id');
+
+    INSERT INTO tasks.tasks(id, title, author, ch_dt, body)
+    SELECT _task_id,
+           title,
+           author,
+           _dt,
+           body;
+
+    RETURN JSONB_BUILD_OBJECT('task_id', _task_id,
+                              'task_title', title);
+END;
+$$;
